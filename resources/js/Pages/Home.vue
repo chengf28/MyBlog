c1dac561ac61219c92c95062281298a44e24c0f0
天才只尿裤 <guifei@guifeideMacBook-Air.local>
2020-03-27 12:02:31 +0800

    新增文章面板

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -17,68 +17,8 @@
             </div>
         </section>
         <section id="content_display">
-            <div class="container pannel_container">
-                <div class="ctx_pannel">
-                    <div class="ctx_title">
-                        <p>ABOUT ME</p>
-                        <span class="time">2020年3月6日00:36:17</span>
-                    </div>
-                    <div class="ctx_body">
-                        <p>This can be an about section.</p>
-                        <p>Impossible considered invitation him men instrument saw celebrated unpleasant. Put rest and must set kind next many near nay. He exquisite continued explained middleton am. Voice hours young woody has she think equal. Estate moment he at on wonder at season little. Six garden result summer set family esteem nay estate. End admiration mrs unreserved discovered comparison especially invitation.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                    </div>
-                    <div class="ctx_foot">
-                        <a class="button" href="#">
-                            <span class="iconfont icon-right"></span>
-                        </a>
-                    </div>
-                </div>
-                <div class="ctx_pannel">
-                    <div class="ctx_title">
-                        <p>ABOUT ME</p>
-                        <span class="time">2020年3月6日00:36:17</span>
-                    </div>
-                    <div class="ctx_body">
-                        <p>This can be an about section.</p>
-                        <p>Impossible considered invitation him men instrument saw celebrated unpleasant. Put rest and must set kind next many near nay. He exquisite continued explained middleton am. Voice hours young woody has she think equal. Estate moment he at on wonder at season little. Six garden result</p>
-                    </div>
-                    <div class="ctx_foot">
-                        <a class="button" href="#">
-                            <span class="iconfont icon-right"></span>
-                        </a>
-                    </div>
-                </div>
-                <div class="ctx_pannel">
-                    <div class="ctx_title">
-                        <p>静夜思</p>
-                        <span class="time">2020年3月6日00:36:17</span>
-                    </div>
-                    <div class="ctx_body">
-                        <p>This can be an about section.</p>
-                        <p>Impossible considered invitation him men instrument saw celebrated unpleasant. Put rest and must set kind next many near nay. He exquisite continued explained middleton am. Voice hours young woody has she think equal. Estate moment he at on wonder at season little. Six garden result summer set family esteem nay estate. End admiration mrs unreserved discovered comparison especially invitation.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                        <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
-                    </div>
-                    <div class="ctx_foot">
-                        <a class="button" href="#">
-                            <span class="iconfont icon-right"></span>
-                        </a>
-                    </div>
-                </div>
+            <div class="container">
+                <essay-panel :essays="essays"></essay-panel>
             </div>
         </section>
     </div>
@@ -88,10 +28,12 @@
 import { EventBus } from "../even_bus.js";
 import _ from "lodash";
 import PhotoPanel from "../components/global/PhotoPanel";
+import EssayPanel from "../components/global/EssayPanel";
 
 export default {
     components: {
-        PhotoPanel
+        PhotoPanel,
+        EssayPanel
     },
     data() {
         return {
@@ -122,29 +64,8 @@ export default {
                     date: "2020年3月6日00:39:26",
                     src: "/storage/img/img2.jpg",
                     desc: "This is a photo description"
-                },{
-                    title: "图1",
-                    date: "2020年3月6日00:39:26",
-                    src: "/storage/img/img1.jpg",
-                    desc: "This is a photo description"
                 },
                 {
-                    title: "图2",
-                    date: "2020年3月6日00:39:26",
-                    src: "/storage/img/img2.jpg",
-                    desc: "This is a photo description"
-                },{
-                    title: "图1",
-                    date: "2020年3月6日00:39:26",
-                    src: "/storage/img/img1.jpg",
-                    desc: "This is a photo description"
-                },
-                {
-                    title: "图2",
-                    date: "2020年3月6日00:39:26",
-                    src: "/storage/img/img2.jpg",
-                    desc: "This is a photo description"
-                },{
                     title: "图1",
                     date: "2020年3月6日00:39:26",
                     src: "/storage/img/img1.jpg",
@@ -156,6 +77,60 @@ export default {
                     src: "/storage/img/img2.jpg",
                     desc: "This is a photo description"
                 },
+                {
+                    title: "图1",
+                    date: "2020年3月6日00:39:26",
+                    src: "/storage/img/img1.jpg",
+                    desc: "This is a photo description"
+                },
+                {
+                    title: "图2",
+                    date: "2020年3月6日00:39:26",
+                    src: "/storage/img/img2.jpg",
+                    desc: "This is a photo description"
+                },
+                {
+                    title: "图1",
+                    date: "2020年3月6日00:39:26",
+                    src: "/storage/img/img1.jpg",
+                    desc: "This is a photo description"
+                },
+                {
+                    title: "图2",
+                    date: "2020年3月6日00:39:26",
+                    src: "/storage/img/img2.jpg",
+                    desc: "This is a photo description"
+                }
+            ],
+            essays: [
+                {
+                    title: "About Me",
+                    date: "2020年3月6日00:39:26",
+                    content: `<p>This can be an about section.</p>
+                <p>Impossible considered invitation him men instrument saw celebrated unpleasant. Put rest and must set kind next many near nay. He exquisite continued explained middleton am. Voice hours young woody has she think equal. Estate moment he at on wonder at season little. Six garden result summer set family esteem nay estate. End admiration mrs unreserved discovered comparison especially invitation.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>`
+                }, {
+                    title: "About Me",
+                    date: "2020年3月6日00:39:26",
+                    content: `<p>This can be an about section.</p>
+                <p>Impossible considered invitation him men instrument saw celebrated unpleasant. Put rest and must set kind next many near nay. He exquisite continued explained middleton am. Voice hours young woody has she think equal. Estate moment he at on wonder at season little. Six garden result summer set family esteem nay estate. End admiration mrs unreserved discovered comparison especially invitation.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>
+                <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery.</p>`
+                }
+
             ]
         };
     },
---
+++ resources/js/components/global/EssayPanel.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="pannel_container">
+        <div class="essay_pannel" v-for="essay in essays" :key="essay.title">
+            <div class="essay_title">
+                <p>{{essay.title}}</p>
+                <span class="time">{{essay.date}}</span>
+            </div>
+            <div class="essay_body" v-html="essay.content"></div>
+            <div class="essay_foot">
+                <a class="button" href="#">
+                    <span class="iconfont icon-right"></span>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        essays: Array
+    }
+};
+</script>
