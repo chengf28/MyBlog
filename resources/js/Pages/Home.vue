6c4ee7ad9005fbc6c51fc54660f877c63e80cbc9
天才只尿裤 <guifei@guifeideMacBook-Air.local>
2020-04-22 21:35:32 +0800

    update

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,18 +1,44 @@
 <template>
     <div id="home">
-        <div class="banner">
-            天才纸尿裤
-        </div>
+        <section>
+            <nav>
+                <!-- <router-link :to="{name:'home'}" class="nav-item">
+                    <p>首页</p>
+                </router-link>-->
+                <div v-for="(router,index) in routers" :key="index" class="nav-item" @mouseover="over">
+                    <router-link v-bind:to="router.name" >
+                        <p class="nav-name" >{{router.meta.name}}</p>
+                        <div class="desc">
+                            <p class="nav-desc">{{router.meta.descript}}</p>
+                        </div>
+                    </router-link>
+                </div>
+            </nav>
+        </section>
+        <!-- <div class="banner">
+            <div>
+                天才只尿裤
+                <span>NioKu</span>
+            </div>
+        </div>-->
     </div>
 </template>
 
-
 <script>
 export default {
-    data(){
+    data() {
         return {
-            
+            routers: []
         };
+    },
+    mounted() {
+        this.routers = this.$router.options.routes[1].children;
+    },
+    methods: {
+        over(e) {
+            console.log(e.target.children[0]);
+            
+        }
     }
-}
+};
 </script>
--- resources/js/Routes/web.js
+++ resources/js/Routes/web.js
@@ -5,30 +5,50 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 
-import Home from '../Pages/Home';
-import Layout from '../Pages/Layout';
-import PhotoTags from "../Pages/PhotoTags";
 
 export default new VueRouter({
     routes: [
         {
             'path': '/',
             'name': 'home',
-            'component': Home
+            'component': require('#/Home').default
         },
         {
             'path': 'layout',
             'name': 'layout',
-            'component': Layout,
+            'component': require('#/Layout').default,
             'children': [
                 {
+                
                     'path': 'photo',
                     'name': 'photo',
-                    'component': PhotoTags
+                    'component': require('#/PhotoTags').default,
+                    'meta': {
+                        'name': '光线 、影 、色彩',
+                        'descript':'随便乱拍'
+                    }
+                    // }, {
+                    //     'path': 'photos/:tagid',
+                    //     'name': 'photos',
+                    //     'component': require('#/Photos').default,
+                    // },{
                 }, {
-                    'path': 'photos/:tagid',
-                    'name': 'photos',
-                    'component': require('#/Photos').default,
+                    'path': 'code',
+                    'name': 'code',
+                    'component': require('#/PhotoTags').default,
+                    'meta': {
+                        'name': '零与一',
+                        'descript':'编程的世界,其实就是简单的0与1'
+                    }
+                },
+                {
+                    'path': 'paper',
+                    'name': 'paper',
+                    'component': require('#/PhotoTags').default,
+                    'meta': {
+                        'name': '二十一克',
+                        'descript':'人的灵魂只有21克'
+                    }
                 }
             ]
         }
